7fca7fe263b32d1b907449b3078c41173a5d948e
sawanoboly <sawanoboriyu@higanworks.com>
2015-05-07 20:05:38 +0900

    initial config

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'fb_graph'
+gem 'sinatra'
+
+group :development do
+  gem 'foreman'
+  gem 'shotgun'
+  gem 'pry'
+  gem 'pry-doc'
+end
+
---
+++ app.rb
@@ -0,0 +1,17 @@
+# coding: utf-8
+require 'fb_graph'
+require 'sinatra/base'
+require 'uri'
+
+class FBPoster < Sinatra::Base
+  get '/fbg/:page_id/:token' do |page_id, token|
+    @group = FbGraph::Group.new(params[:page_id], access_token: params[:token])
+    res = @group.feed! message: 'getchef blogの更新 via feedposter from zapier', link: params[:link]
+  end
+
+  get '/debug/:page_id/:token' do |page_id, token|
+    @group = FbGraph::Group.new(ENV['FB_GID'], access_token: ENV['FB_ACCESS_TOKEN'])
+    res = @group.feed! message: 'getchef blogの更新 via feedposter from zapier', link: params[:link]
+    puts res.to_s
+  end
+end
